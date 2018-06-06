2dd2f536f6a2eadb8a799e9f7cbdc1c43a47c1b5
Changmin Lim <chang.lim92@gmail.com>
2018-06-06 20:43:26 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,6 +18,6 @@ class String
     # self.split.delete_if {|i|
     #   i == "!" || i == "." || i == ","
     #   }.count 
-    self.split.gsub(/[^A-Za-z0-9\s]/i, '').count 
+    gsub(/[^A-Za-z0-9\s]/i, ' ')
   end
 end
